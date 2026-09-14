77bfbb00f3b194592cde49555bfde1994f17f831
William Stoneham <william.stoneham04@gmail.com>
2026-09-14 10:35:05 +0100

    added a pnpm clean step

--- poc.sh
+++ poc.sh
@@ -2,6 +2,7 @@
 
 # This script showcases how bun courrupts a package.json file when it tries to move a pnpm-workspace.yaml into the package.json
 
+pnpm clean --lockfile
 pnpm install
 
 # This is the original package.json
